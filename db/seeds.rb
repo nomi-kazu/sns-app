fca276c6cfdb33cbc993cae80746c072504bf97b
nomi-kazu <nomikazyu@gmail.com>
2021-12-24 10:32:47 +0900

    feat: 記事一覧を取得

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Article.create({title: '記事1', content: '素晴らしい記事1'})
+Article.create({title: '記事2', content: '素晴らしい記事です2'})
