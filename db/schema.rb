1d2b5b5bbf1b924531929e8d6f7ad10f9af0a441
nomi-kazu <nomikazyu@gmail.com>
2021-12-29 11:07:26 +0900

    feat: ユーザーと記事のリレーションを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_002701) do
+ActiveRecord::Schema.define(version: 2021_12_29_013315) do
 
   create_table "articles", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
