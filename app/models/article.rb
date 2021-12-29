f93eef9b0c020455a5ed93660eef9fbb89a81918
nomi-kazu <nomikazyu@gmail.com>
2021-12-29 21:24:04 +0900

    fix: DBの修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,11 +3,11 @@
 # Table name: articles
 #
 #  id         :bigint           not null, primary key
-#  content    :text(65535)
-#  title      :string(255)
+#  content    :text(65535)      not null
+#  title      :string(255)      not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  user_id    :bigint
+#  user_id    :bigint           not null
 #
 # Indexes
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_013315) do
+ActiveRecord::Schema.define(version: 2021_12_24_000305) do
 
   create_table "articles", charset: "utf8mb4", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
