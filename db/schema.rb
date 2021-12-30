c831a85e22d277d3b5ba28f1131b8d51d83cd793
nomi-kazu <nomikazyu@gmail.com>
2021-12-30 11:38:21 +0900

    feat: commentモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_24_000305) do
+ActiveRecord::Schema.define(version: 2021_12_29_124025) do
 
   create_table "articles", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2021_12_24_000305) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "article_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
