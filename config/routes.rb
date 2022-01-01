d291ecc6ab0fe5e59ec985a3faa077a70d3e3cf8
nomi-kazu <nomikazyu@gmail.com>
2022-01-01 19:49:04 +0900

    feat: likeのcreate,destroyアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   resources :articles do
     resources :comments, only: %i[new create]
+
+    resource :like, only: %i[create destroy]
   end
 
   resource :profile, only: %i[show edit update]
