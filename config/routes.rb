02176b5eae589a6efbb3939bcdbba19ef64d6d27
nomi-kazu <nomikazyu@gmail.com>
2022-01-04 23:38:11 +0900

    feat: follow,unfollow機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
     resource :like, only: %i[create destroy]
   end
 
-  resources :accounts, only: [:show]
+  resources :accounts, only: [:show] do
+    resources :follows, only: [:create]
+    resources :unfollows, only: [:create]
+  end
 
   resource :profile, only: %i[show edit update]
   resources :favorites, only: %i[index]
