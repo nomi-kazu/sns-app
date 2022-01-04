384893043dffe31367b7661b9187785d2003adfb
nomi-kazu <nomikazyu@gmail.com>
2022-01-04 19:31:47 +0900

    feat: フォロー機能を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,12 @@ class User < ApplicationRecord
   has_many :favorite_articles, through: :likes, source: :article
   has_one :profile, dependent: :destroy
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   delegate :birthday, :gender, :age, to: :profile, allow_nil: true
 
   def has_written?(article)
@@ -35,6 +41,15 @@ class User < ApplicationRecord
 
   def has_liked?(article)
     likes.exists?(article_id: article.id)
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
   end
 
   def display_name
